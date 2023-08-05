c2464caaa3977bb14c00c576b4ad5fec9b89803d
walidsoufiani <140754926+walidsoufiani@users.noreply.github.com>
2023-08-05 16:40:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -120,14 +120,7 @@ def generate_answer(question,openAI_key):
     for c in topn_chunks:
         prompt += c + '\n\n'
         
-    prompt += "Instructions: Compose a comprehensive reply to the query using the search results given. "\
-              "Cite each reference using [number] notation (every result has this number at the beginning). "\
-              "Citation should be done at the end of each sentence. If the search results mention multiple subjects "\
-              "with the same name, create separate answers for each. Only include information found in the results and "\
-              "don't add any additional information. Make sure the answer is correct and don't output false content. "\
-              "If the text does not relate to the query, simply state 'Found Nothing'. Ignore outlier "\
-              "search results which has nothing to do with the question. Only answer what is asked. The "\
-              "answer should be short and concise.\n\nQuery: {question}\nAnswer: "
+
     
     prompt += f"Query: {question}\nAnswer:"
     answer = generate_text(openAI_key, prompt,"text-davinci-003")
